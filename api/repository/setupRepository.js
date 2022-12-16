606e592aad4e95bdfceccd414fb5a5ea00c3a258
Rick in 't veld <rick.in.t.veld@opinity.nl>
2022-12-16 16:48:44 +0100

    Created the setup repository to post the new setup to the api

---
+++ api/repository/setupRepository.js
@@ -0,0 +1,32 @@
+import config from "../../config.json" assert { type: "json" };
+
+const setupRepository = { insert };
+
+async function insert(username) {
+  const date = new Date();
+
+  const body = {
+    username: username,
+    date: date.toISOString(),
+  };
+
+  const headers = {
+    "Content-Type": "application/json",
+    "Bot-Type": "SMC_DISCORD_BOT",
+    "Bot-Key": config.guildId,
+  };
+
+  const requestOptions = {
+    method: "POST",
+    headers: headers,
+    body: JSON.stringify(body),
+  };
+
+  try {
+    await fetch("http://localhost:3001/user/insert-setup", requestOptions);
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+export default setupRepository;
